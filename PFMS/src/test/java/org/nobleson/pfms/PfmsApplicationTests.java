0c1e041139a77e440e8a413d7554adef05ec7e8c
NOBLE ESELASE VULLEY <nevulley@st.ug.edu.gh>
2024-04-22 20:20:58 +0000

    PFMS created using Springboot

---
+++ PFMS/src/test/java/org/nobleson/pfms/PfmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nobleson.pfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
